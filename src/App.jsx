1ad447b9ab01596247f7533cfed902412335c043
NIGHT HHOR <nightohr4@gmail.com>
2025-07-13 15:43:09 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-violet-400">Welcome to DrobCart</h1>
+      <p className="mt-4 text-lg text-gray-300">Your free dropshipping website is live!</p>
+    </div>
+  );
+}
+
+export default App;
